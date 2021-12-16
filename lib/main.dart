24a4ccf9881bd665110cd983bd83c8f3d38cbb26
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2021-12-16 10:15:14 -0600

    Add initial layout
    Add initial layout and add new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,36 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('images/tokyo-ghoul.jpg'),
+                ),
+                Text('Anthony Campbell',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'SOFTWARE ENGINEER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+        ),
       ),
     ),
   );
 }
+
